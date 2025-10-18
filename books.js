c0bcb46ab57b81893c856ba3d57d4a4602ed8742
Eddi Perz F <eddi@flowneuroscience.com>
2025-10-19 01:43:14 +0200

    WIP

--- books.js
+++ books.js
@@ -1,19 +1,42 @@
-Book.prototype.price = 20;
+const my_library = [];
 
-function Book(name, author, read, pages){
- 
+function Book(name, author, pages, id){
+    if(!new.target){
+        throw Error("You must use the 'new operator to call this function")
+    }
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.id = id;
     this.info = function() {
-        var returnString = `${this.name} by ${this.author}, ${this.pages}. ${this.price}`;
-        if(this.read == true) returnString += ", read."
-        else returnString +=", not read yet."
+        var returnString = `${this.name} by \n ${this.author}. ${this.pages} pages long.`;
         return returnString;
-    }
+    };
 }
 
-const hobbit = new Book("The hobbit", "JRR tolkien", false, "295");
-console.log(hobbit.info());
+function add_book_to_library(name, author, pages){
+    const book = new Book(name, author, pages, crypto.randomUUID());
+    my_library.push(book);
+}
+
+function remove_book_from_library(){
+    // button event from book div to pop/remove it from library
+}
+
+add_book_to_library("Lord of the schlajna", "Eddi Perz", 666);
+add_book_to_library("Lord of the schlajna", "Eddi Perz", 669);
+
+// Create library div
+const library_div = document.querySelector(".library");
+
+
+for(var book of my_library){
+    // Create the book-div and add content
+    var book_div = document.createElement("div")
+    book_div.className = "book";
+    book_div.textContent = book.info();
+
+    // Add book to library
+    library_div.appendChild(book_div);
+}
 
